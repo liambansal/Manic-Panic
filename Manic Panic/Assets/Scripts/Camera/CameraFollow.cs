8a3ebe215002036293800b4b09aa9b8641e87b58
RadiantGamesInc <58471521+RadiantGamesInc@users.noreply.github.com>
2019-12-17 14:06:58 +0000

    Initial Commit
    A protoype containing player movement, basic tilemap obstacles, camera movement and controller support

---
+++ Manic Panic/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject player = null;
+
+	private Rigidbody2D cameraRigidbody;
+
+	private int moveForce = 1;
+
+    void Start() {
+		cameraRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+		if (transform.position.y <= (player.transform.position.y - 2)) {
+			cameraRigidbody.AddForce((Vector2.up * moveForce), ForceMode2D.Force);
+		} else if (transform.position.y >= (player.transform.position.y + 2))
+		{
+			cameraRigidbody.AddForce((Vector2.down * moveForce), ForceMode2D.Force);
+		}
+
+		if ((transform.position.y >= (player.transform.position.y - 1)) && (transform.position.y <= (player.transform.position.y + 1)))
+		{
+			cameraRigidbody.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ Manic Panic/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField]
+	private string controllerPrefix = "";
+
+	private RaycastHit2D ray;
+
+	private enum Movement { Up, Left, Down, Right, Jump };
+
+	const int moveDistance = 1;
+	const int jumpDistance = 2;
+
+	private bool canMoveVertically = true;
+	private bool canMoveHorizontally = true;
+	private bool canJump = true;
+
+	void Update() {
+		if (Input.GetAxis(controllerPrefix + "Vertical") == 0) {
+			canMoveVertically = true;
+		}
+
+		if (Input.GetAxis(controllerPrefix + "Horizontal") == 0) {
+			canMoveHorizontally = true;
+		}
+
+		if (Input.GetAxis(controllerPrefix + "Jump") == 0)
+		{
+			canJump = true;
+		}
+
+		if ((Input.GetAxis(controllerPrefix + "Vertical") > 0) && (canMoveVertically)) {
+			CheckMoveDirection(Movement.Up);
+			canMoveVertically = false;
+		}
+
+		if ((Input.GetAxis(controllerPrefix + "Horizontal") < 0) && (canMoveHorizontally)) {
+			CheckMoveDirection(Movement.Left);
+			canMoveHorizontally = false;
+		}
+
+		if ((Input.GetAxis(controllerPrefix + "Vertical") < 0) && (canMoveVertically)) {
+			CheckMoveDirection(Movement.Down);
+			canMoveVertically = false;
+		}
+
+		if ((Input.GetAxis(controllerPrefix + "Horizontal") > 0) && (canMoveHorizontally)) {
+			CheckMoveDirection(Movement.Right);
+			canMoveHorizontally = false;
+		}
+
+		if ((Input.GetAxis(controllerPrefix + "Jump") > 0) && (canJump))
+		{
+			CheckMoveDirection(Movement.Jump);
+			canJump = false;
+		}
+	}
+
+	private void CheckMoveDirection (Movement moveDirection) {
+		switch (moveDirection) {
+			case Movement.Up: {
+				ray = Physics2D.Raycast(gameObject.transform.position, Vector2.up, moveDistance, 8);
+
+				if (!ray.collider) {
+					Move(moveDirection);
+				}
+
+				break;
+			}
+			case Movement.Left: {
+				ray = Physics2D.Raycast(gameObject.transform.position, Vector2.left, moveDistance, 8);
+
+				if (!ray.collider) {
+					Move(moveDirection);
+				}
+
+				break;
+			}
+			case Movement.Down: {
+				ray = Physics2D.Raycast(gameObject.transform.position, Vector2.down, moveDistance, 8);
+
+				if (!ray.collider) {
+					Move(moveDirection);
+				}
+
+				break;
+			}
+			case Movement.Right: {
+				ray = Physics2D.Raycast(gameObject.transform.position, Vector2.right, moveDistance, 8);
+
+				if (!ray.collider) {
+					Move(moveDirection);
+				}
+
+				break;
+			}
+			case Movement.Jump:
+			{
+				ray = Physics2D.Raycast(gameObject.transform.position, Vector2.up, jumpDistance, 8);
+
+				if (!ray.collider)
+				{
+					Move(moveDirection);
+				}
+
+				break;
+			}
+		}
+	}
+
+	private void Move(Movement moveDirection) {
+		switch (moveDirection) {
+			case Movement.Up: {
+				gameObject.transform.Translate((Vector2.up * moveDistance), Space.World);
+				break;
+			}
+			case Movement.Left: {
+				gameObject.transform.Translate((Vector2.left * moveDistance), Space.World);
+				break;
+			}
+			case Movement.Down: {
+				gameObject.transform.Translate((Vector2.down * moveDistance), Space.World);
+				break;
+			}
+			case Movement.Right: {
+				gameObject.transform.Translate((Vector2.right * moveDistance), Space.World);
+				break;
+			}
+			case Movement.Jump:
+			{
+				gameObject.transform.Translate((Vector2.up * jumpDistance), Space.World);
+				break;
+			}
+		}
+	}
+}
