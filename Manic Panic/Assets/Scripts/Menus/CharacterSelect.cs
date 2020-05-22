66f7752706b633761ba94cb4adc642f1a861d6fa
liambansal <55756009+liambansal@users.noreply.github.com>
2020-05-22 01:05:17 +0100

    +Main Menu, +Character Select Screen
    Additions
    + Implemented part of the functionality for the main menu and character selection screens.
    
    Changes
    ~ Organised the file structure of the Unity project.

---
+++ Manic Panic/Assets/Scripts/Menus/CharacterSelect.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour {
+	// We need at least two players to play against each other.
+	private const int minimumPlayers = 2;
+	// We only support up to four players.
+	private const int maximumPlayers = 4;
+	// How many people are playing.
+	private int playerCount = 0;
+
+	// Which players are playing. Represents player 1 through 4.
+	private bool[] players = new bool[4] { false, false, false, false };
+	// Which players have selected a character.
+	private bool[] characterSelected = new bool[4] { false, false, false, false };
+
+	// Matching prefixes for project settings inputs.
+	private string[] playerPrefixes = new string[4] { "P1", "P2", "P3", "P4"}; 
+
+	/// <summary>
+	/// Checks for player input to find out who wants to play and loads the game scene when everyone 
+	/// has selected a character.
+	/// </summary>
+	private void Update() {
+		HandleInput();
+		ReadyPlayers();
+	}
+
+	private void HandleInput() {
+		for (int i = 0; i < maximumPlayers; ++i) {
+			// If the player hasn't already opted in to play...
+			if (players[i] == false) {
+				// ...check if they want to play.
+				if (Input.GetAxis(playerPrefixes[i] + "Jump") > 0.0f) {
+					players[i] = true;
+					playerCount++;
+				}
+			} else { // They already opted in to play.
+					 // Check for confirmation of their character.
+				if (Input.GetAxis(playerPrefixes[i] + "Jump") > 0.0f) {
+					characterSelected[i] = true; ;
+				}
+			}
+		}
+	}
+
+	private void ReadyPlayers() {
+		int playersReady = 0;
+
+		// Checks if all the players have selected a character and confirms another player is ready.
+		for (int i = 0; i < maximumPlayers; ++i) {
+			if (characterSelected[i] == true) {
+				++playersReady;
+			}
+		}
+
+		// If we at least have the minimum number of players to play...
+		if (playerCount >= minimumPlayers) {
+			// ...check they're all ready.
+			if (playersReady == playerCount) {
+				SceneManager.LoadScene("Level One", LoadSceneMode.Single);
+			}
+		}
+	}
+}
---
+++ Manic Panic/Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+	private void PlayGame() {
+		// Load the next scene in the build index.
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	private void OpenSettings() {
+
+	}
+
+	private void LoadCredits() {
+		SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+	}
+
+	private void QuitApplication() {
+		// Close the application.
+		Application.Quit();
+	}
+}
